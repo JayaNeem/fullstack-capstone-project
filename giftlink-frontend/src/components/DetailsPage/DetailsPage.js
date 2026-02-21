f9681774b11f10d16ee6f08217b88a4b9bc53b1e
JayaNeem <jaya.neem@gmail.com>
2026-02-21 00:42:03 -0500

    Removed unused variables

--- giftlink-frontend/src/components/DetailsPage/DetailsPage.js
+++ giftlink-frontend/src/components/DetailsPage/DetailsPage.js
@@ -39,7 +39,7 @@ function DetailsPage() {
 		// Task 3: Scroll to top on component mount
 		window.scrollTo(0, 0);
 
-    }, [productId]);
+    }, [productId, navigate]);
 
 
     const handleBackClick = () => {
@@ -91,7 +91,6 @@ return (
                             <div className="no-image-available-large">No Image Available</div>
                         )}
                     </div>
-                    // Task 6: Display gift details
                     	<p><strong>Category:</strong> 
 				{gift.category}
 			</p>
